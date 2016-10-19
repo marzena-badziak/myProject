a3d0894c29ac7c1c0cceb4de9d68a169cb64899d
marzena-badziak <marzena.badziak@gmail.com>
2016-10-19 09:35:53 +0200

    pierwszy commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var fruits = ["truskawka", "banan", "mango", "czeresnia"]
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+
+for (var i=0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
